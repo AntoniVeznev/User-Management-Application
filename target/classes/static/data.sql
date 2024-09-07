d43a0ec99d1bc8bf78331608c47e98d21ba01c63
Antoni Veznev <antoniveznev@gmail.com>
2024-09-07 12:03:48 +0300

    Removing some files.

--- target/classes/static/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `users`
-    (`id`, `first_name`, `last_name`, `date_of_birth`, `phone_number`, `email`)
-VALUES
-    (1, "Antoni", "Veznev", '1989-6-16', "0886379458", "antoni_1989@abv.bg"),
-    (2, "Svilen", "Veznev", '1957-9-10', "0892214244", "svilen_1957@abv.bg"),
-    (3, "Krasia", "Vezneva", '1960-2-02', "0892753359", "krasia_1960@abv.bg"),
-    (4, "Ivan", "Ivanov", '1995-1-15', "0884564658", "ivan_1995@abv.bg"),
-    (5, "Gosho", "Goshev", '2000-4-27', "0993424235", "gosho_2000@abv.bg");
