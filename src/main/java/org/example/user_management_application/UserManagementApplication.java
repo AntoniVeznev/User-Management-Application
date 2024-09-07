afe63ac49574b80f9ce4816db452c4ba42207663
Antoni Veznev <antoniveznev@gmail.com>
2024-09-07 17:01:31 +0300

    Adding Swagger!

--- src/main/java/org/example/user_management_application/UserManagementApplication.java
+++ src/main/java/org/example/user_management_application/UserManagementApplication.java
@@ -1,9 +1,29 @@
 package org.example.user_management_application;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User management application",
+                version = "1.0.0",
+                description = "This project is made for Markovski-solutions",
+                termsOfService = "",
+                contact = @Contact(
+                        name = "Antoni Veznev",
+                        email = "antoniveznev@gmail.com"
+                ),
+                license = @License(
+                        name = "license",
+                        url = "Antoni Veznev"
+                )
+        )
+)
 public class UserManagementApplication {
 
     public static void main(String[] args) {
